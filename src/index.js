653017874c8868610b94624ac518159a924769a5
Pepper Herman <pepper.herman@hva.nl>
2022-08-06 00:05:58 +0200

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
-import App from "./App";
-import "./style.css";
-import {createRoot} from "react-dom/client";
-
-
-
-createRoot(document.getElementById("root")).render(<App />);
